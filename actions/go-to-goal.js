54ac86fb7b9b9818a7c172a45eb848a186902ead
Rakesh Pai <rakeshpai@gmail.com>
2017-09-25 09:53:01 +0530

    Cleanup

--- actions/go-to-goal.js
+++ actions/go-to-goal.js
@@ -1,20 +1,18 @@
 const { createAction } = require("../utils");
-const config = require("../config");
+const { topSpeed, steeringPid } = require("../config");
 const PID = require("../utils/pid");
 
-const isWithin = (range, a) => b => ((a.x - b.x) ^ 2) + ((a.y - b.y) ^ 2) < (range ^ 2);
+const square = num => Math.pow(num, 2);
 
-const halt = { speed: 0, steering: 0 };
+const isWithin = (range, a) => b => (square(a.x - b.x) + square(a.y - b.y)) < square(range);
 
 module.exports = createAction({
   name: "Go to goal",
   action: (goal = ({x:0, y:0}), done) => {
-    const startTime = Date.now();
-
     const isCloseBy = isWithin(300, goal);
     const hasReached = isWithin(30, goal);
 
-    const phiDesiredPID = new PID(0.5);
+    const phiDesiredPID = new PID(...steeringPid);
     phiDesiredPID.angle = true;
 
     let isGoalSet = false;
@@ -27,16 +25,12 @@ module.exports = createAction({
 
       if(hasReached(odometry)) {
         done();
-        return halt;
+        return { speed: 0, steering: 0 };
       }
 
-      const speed = isCloseBy(odometry) ? (config.topSpeed * 0.8) : config.topSpeed;
-      const steering = phiDesiredPID.update(odometry.phi);
-
-      if(Date.now() - startTime < 5000) {
-        return { speed, steering };
-      } else {
-        return halt;
+      return {
+        speed: isCloseBy(odometry) ? (topSpeed * 0.8) : topSpeed,
+        steering: phiDesiredPID.update(odometry.phi)
       }
     });
   }
