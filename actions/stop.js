673d35b7afec0369027f336feaf7579c33141b50
Viresh S <asviresh@gmail.com>
2017-09-27 09:43:01 +0530

    force stop

--- actions/stop.js
+++ actions/stop.js
@@ -2,16 +2,20 @@ const { createAction } = require('../utils/behaviour-engine');
 
 module.exports = createAction({
   name: 'Stop',
-  action: done => {
+  action: ({ emergency=false }, done) => {
     const startTime = Date.now();
 
     return sensors => {
-      if(
+      if(emergency == true) {
+        return { speed: 0, steering: 0, { emergency: true } }
+      } else if (
         Date.now() - startTime > 500
         && sensors.raw.left === 0 && sensors.raw.right === 0
-      ) done();
+      ) {
+        done();
+        return { speed: 0, steering: 0 }
+      }
 
-      return { speed: 0, steering: 0 }
     };
   }
 });
